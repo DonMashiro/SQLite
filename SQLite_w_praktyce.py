73fc3a7cef616f973cf6081df1067ae95d0249a6
DonMashiro <dawid.masiarek@adres.pl>
2024-11-11 20:06:13 +0100

    Update SQLite_w_praktyce.py
    Poprawa kodu zgodnie z uwagami:
    - dlaczego dwukrotnie jest __main__?
    - w kodzie widzę tylko tworzenie tabel i dodawanie rekordów, w przykładowych kodach w module użyto dużo więcej funkcji, gdzie znajdowanie rekordów, aktualizacja, usuwanie?

--- SQLite_w_praktyce.py
+++ SQLite_w_praktyce.py
@@ -17,8 +17,83 @@ def execute_sql(conn, sql):
    except Error as e:
        print(e)
 
-if __name__ == "__main__":
+def select_all(conn, table):
+   """
+   Query all rows in the table
+   :param conn: the Connection object
+   :return:
+   """
+   cur = conn.cursor()
+   cur.execute(f"SELECT * FROM {table}")
+   rows = cur.fetchall()
 
+   return rows
+
+def select_where(conn, table, **query):
+   """
+   Query tasks from table with data from **query dict
+   :param conn: the Connection object
+   :param table: table name
+   :param query: dict of attributes and values
+   :return:
+   """
+   cur = conn.cursor()
+   qs = []
+   values = ()
+   for k, v in query.items():
+       qs.append(f"{k}=?")
+       values += (v,)
+   q = " AND ".join(qs)
+   cur.execute(f"SELECT * FROM {table} WHERE {q}", values)
+   rows = cur.fetchall()
+   return rows
+
+def delete_where(conn, table, **kwargs):
+    """
+    Delete from table where attributes from kwargs.
+    :param conn: Connection to the SQLite database
+    :param table: table name
+    :param kwargs: dict of attributes and values
+    :return:
+    """
+    qs = []
+    values = tuple()
+    for k, v in kwargs.items():
+        qs.append(f"{k}=?")
+        values += (v,)
+    q = " AND ".join(qs)
+
+    sql = f'DELETE FROM {table} WHERE {q}'
+    cur = conn.cursor()
+    cur.execute(sql, values)
+    conn.commit()
+    print(f"Deleted from {table}")
+
+def update(conn, table, name, **kwargs):
+    """
+    Update number of repetitions of the exercise
+    :param conn: the Connection object
+    :param table: table name
+    :param name: name of the exercise to update
+    :return:
+    """
+    parameters = [f"{k} = ?" for k in kwargs]
+    parameters = ", ".join(parameters)
+    values = tuple(v for v in kwargs.values())
+    values += (name, )
+
+    sql = f''' UPDATE {table}
+               SET {parameters}
+               WHERE name = ?'''
+    try:
+        cur = conn.cursor()
+        cur.execute(sql, values)
+        conn.commit()
+        print("Update successful")
+    except sqlite3.OperationalError as e:
+        print(f"Error updating record: {e}")
+
+def create_tables(conn):
    create_trainings_sql = """
    -- trainings table
    CREATE TABLE IF NOT EXISTS trainings (
@@ -81,21 +156,29 @@ def add_exercise(conn, exercise):
   return cur.lastrowid
 
 if __name__ == "__main__":
-    training = ("biceps", "2024-09-20 10:00:00", "2024-09-20 11:30:00")
+    training = ("biceps", "2024-09-20 10:00:00", "2024-09-20 12:00:00")
     
     conn = create_connection("database_2.db")
     tr_id = add_training(conn, training)
 
-    exercise = (tr_id,
-                "cable curls",
-                4,
-                12, 
-                "finished",
-                "2024-09-20 10:00:00",
-                "2024-09-20 10:30:00"
-    )
+    exercises = [(tr_id, "cable curls", 4, 12, "finished", "2024-09-20 10:00:00", "2024-09-20 10:30:00" ),
+                (tr_id, "hammer curls", 4, 12, "finished", "2024-09-20 10:35:00", "2024-09-20 11:00:00"),
+                (tr_id, "EZ-Bar Curl", 4, 12, "finished", "2024-09-20 11:05:00", "2024-09-20 11:30:00"),
+                (tr_id, "chin up", 3, 15, "ongoing", "2024-09-20 11:35:00", "2024-09-20 12:00:00")]
     
-    exercise_id = add_exercise(conn, exercise)
+    for exercise in exercises:
+        exercise_id = add_exercise(conn, exercise)
     
-    print(tr_id, exercise_id)
-    conn.commit()
+ 
+    #Update number of repetitions of the exercise 'EZ-Bar Curl' from 12 to 15
+    update(conn, "exercises", "EZ-Bar Curl", number_of_rep=15)
+
+    #Delete the exercise 'chin up' directly
+    delete_where(conn, "exercises", name="chin up")
+
+    print("All exercises:")
+    all_exercises = select_all(conn, "exercises")
+    for row in all_exercises:
+        print(row)
+    
+    conn.close()
